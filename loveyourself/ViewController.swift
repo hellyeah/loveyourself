5807fb4e96a24e9285f54699382e615a34ea77bf
hellyeah <dave@hackmatch.com>
2016-05-27 13:54:05 +0700

    add pan gesture recognizer to test out how to animate at will

--- loveyourself/ViewController.swift
+++ loveyourself/ViewController.swift
@@ -8,12 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
     @IBOutlet weak var animatingView: UIView!
     @IBOutlet weak var animatingLabel: UILabel!
 
     @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+
+    var animatingViewOriginalCenter: CGPoint!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //animatingView.
+        //print(animatingLabel.center.x)
+        //print(view.bounds.width)
+        animatingView.center.x  -= view.bounds.width //doesnt work
+        //print(animatingLabel.center.x)
+
+
+
+        // The onCustomTap: method will be defined in Step 3 below.
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(self.onCustomPan(_:)))
+        //var panGestureRecognizer = UIPanGestureRecognizer(target: self, action: "onCustomPan:")
+        // set delegate
+        panGestureRecognizer.delegate = self
+        // Attach it to a view of your choice. If it's a UIImageView, remember to enable user interaction
+        animatingView.userInteractionEnabled = true
+        animatingView.addGestureRecognizer(panGestureRecognizer)
+    }
+
+    func onCustomPan(sender: UIPanGestureRecognizer) {
+        let point = sender.locationInView(view)
+        let velocity = sender.velocityInView(view)
+        let translation = sender.translationInView(view)
+
+        animatingViewOriginalCenter = point
+
+        if sender.state == UIGestureRecognizerState.Began {
+            print("Gesture began at: \(point)")
+        } else if sender.state == UIGestureRecognizerState.Changed {
+            print("Gesture changed at: \(point)")
+            animatingView.center = CGPoint(x: animatingViewOriginalCenter.x + translation.x, y: animatingViewOriginalCenter.y + translation.y)
+        } else if sender.state == UIGestureRecognizerState.Ended {
+            print("Gesture ended at: \(point)")
+        }
+    }
+
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
 
     @IBAction func animateViewWithLabel(sender: AnyObject) {
         // 1
@@ -28,15 +73,6 @@ class ViewController: UIViewController {
 
         // 3
         leadingConstraint = newConstraint
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        //animatingView.
-        //print(animatingLabel.center.x)
-        //print(view.bounds.width)
-        animatingView.center.x  -= view.bounds.width
-        //print(animatingLabel.center.x)
     }
 
     override func didReceiveMemoryWarning() {
