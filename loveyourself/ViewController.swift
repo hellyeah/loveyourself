169e47485d3fd1e1b358dcb6245074dc1cbc0c16
hellyeah <dave@hackmatch.com>
2016-05-27 12:47:41 +0700

    Initial Commit

---
+++ loveyourself/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  loveyourself
+//
+//  Created by David Fontenot on 5/27/16.
+//  Copyright © 2016 David Fontenot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
